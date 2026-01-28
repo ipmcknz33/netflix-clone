7ccffe99641dd2f661842ad425791b5d6f8ba75a
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-27 20:06:05 -1000

    lost webpage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,10 @@ import "./Home.css";
 import NavBar from "../../components/NavBar/NavBar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
@@ -14,11 +18,28 @@ const Home = () => {
           <img src={hero_title} alt="" className="caption-img" />
           <p>
             Discover his ties to a secrect order, a young man living in modern
-            Istanbul embarks on a quest to save the city from an imortal
-            enemy.
+            Istanbul embarks on a quest to save the city from an imortal enemy.
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
         </div>
       </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movie"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Picks For You"} />
+      </div>
+      <Footer />
     </div>
   );
 };
